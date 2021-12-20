3d1283a72374762536cc2ac0327453f3657ead6e
Mofesola Banjo <mofesola.banjo@scoding.com>
2021-12-20 14:15:32 +0200

    Updated build script

--- setup.sh
+++ setup.sh
@@ -6,8 +6,14 @@ docker-compose up -d --build
 
 printf '\nBUILDING FRONTEND APP\n'
 cat <<frontend | docker exec --interactive urlshortener_nginx bash
+    cd /app
+    npm install
+    cp .env.dist .env.production.local
+    npm run build
+    cp -r /app/build /var/www/html
 frontend
 
 printf '\nBUILDING BACKEND APP\n'
 cat <<backend | docker exec --interactive urlshortener_php bash
+    composer install
 backend
